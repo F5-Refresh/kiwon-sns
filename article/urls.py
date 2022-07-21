78b9611802b7d6f7d171ea2925ac140935123895
KIWONY <kien1004@naver.com>
2022-07-21 19:02:30 +0900

    feat: 게시글 리스트 구현(#6)

--- article/urls.py
+++ article/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from article.views import ArticleCreateUpdateAPIView
+from article.views import ArticleCreateUpdateAPIView, ArticleListAPIView
 
 urlpatterns =[
     path('', ArticleCreateUpdateAPIView.as_view(), name ='article'),
-    path('<int:article_id>',ArticleCreateUpdateAPIView.as_view(),name ='article')
+    path('<int:article_id>',ArticleCreateUpdateAPIView.as_view(),name ='article'),
+    path('list',ArticleListAPIView.as_view())
 ]
 
--- article/views.py
+++ article/views.py
@@ -1,20 +1,26 @@
 
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from article.models import Article
-from article.serializers import ArticleCreatePatchSerializer
+from article.serializers import ArticleCreateSerializer, ArticleListSerializer ,ArticlePatchSerializer
 
 
 class ArticleCreateUpdateAPIView(APIView):
-    def post(self, request):
+
+    def post(self, request):    
+        """
+        게시글 생성
+        """
         data = {'user': request.user.id} | request.data
-        print(request.user.id)      # 1
-        print(data)                 # {'user': 1, 'title': '개발', 'content': '개발스', 'hashtag': '#개발개발'}
-        print(request.data)         # {'title': '개발', 'content': '개발스', 'hashtag': '#개발개발'}
-        serializer = ArticleCreatePatchSerializer(data=data)
+        # for i in Article.objects.all():   # ordering test
+        #     print(i.id)
+        # print(request.user.id)      # 1
+        # print(data)                 # {'user': 1, 'title': '개발', 'content': '개발스', 'hashtag': '#개발개발'}
+        # print(request.data)         # {'title': '개발', 'content': '개발스', 'hashtag': '#개발개발'}
+        serializer = ArticleCreateSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -23,16 +29,24 @@ class ArticleCreateUpdateAPIView(APIView):
 
 
     def patch(self, request, article_id):
+        """
+        게시글 수정
+        """
         data = {'user': request.user.id} | request.data
         article = get_object_or_404(Article, id=article_id)
 
-        serializer = ArticleCreatePatchSerializer(data=data, instance= article)
+        serializer = ArticlePatchSerializer(data=data, instance= article)
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleListAPIView(generics.ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleListSerializer
 
 
 
