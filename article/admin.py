90af6d877b3c1173612b3da42516524fcf4cfe3f
KIWONY <kien1004@naver.com>
2022-08-27 18:47:15 +0900

    feat: Article 모델의 admin 페이지 커스텀(#24)

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from article.models import Article, Hashtag
+
+
+admin.site.register(Hashtag)
+
+class ArticleAdmin(admin.ModelAdmin):
+    def user_name(self, obj):
+        return obj.user.name
+
+    def get_hashtag(self, obj):
+        return ',\n'.join([i.hashtag for i in obj.hashtags.all()])
+
+    list_display = ['title','user_name','get_hashtag']
+
+    search_fields = ['title', 'hashtags__hashtag']
+
+admin.site.register(Article, ArticleAdmin)
